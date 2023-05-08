8103aaf438e03816a9e7f3afa09bbe0d47920d53
Rachel Tairum <rachel.tairum@gmail.com>
2023-05-08 09:26:28 -0300

    finaliza lógica de jogo

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ function App() {
   const [pickedWord, setPickedWord] = useState('')
   const [pickedCategory, setPickedCategory] = useState('')
   const [letters, setLetters] = useState([])
+  const [guessedLetters, setGuessedLetters] = useState([])
+  const [wrongLetters, setWrongLetters] = useState([])
+  const [guesses, setGuesses] = useState(3)
+  const [score, setScore] = useState(0)
+
 
   const pickWordAndCategory = () => {
     const categories = Object.keys(words)
@@ -43,18 +48,54 @@ function App() {
     setGameStage(stages[1].name)
   }
 
-  const verifyLetter = () => {
-    setGameStage(stages[2].name)
+  const verifyLetter = (letter) => {
+    console.log(letter);
+    const normalizedLetter = letter.toLowerCase()
+
+    if (
+      guessedLetters.includes(normalizedLetter) 
+      || wrongLetters.includes(normalizedLetter)
+    ){
+      return;
+    }
+
+    if (letters.includes(normalizedLetter)) {
+      setGuessedLetters((actualState) => [
+        ...actualState,
+        normalizedLetter
+      ])
+    } else {
+      setWrongLetters((actualState) => [
+        ...actualState,
+        normalizedLetter
+      ])
+    }
+
+    // setGameStage(stages[2].name)
   }
 
   const retry = () => {
     setGameStage(stages[0].name)
   }
 
+  console.log(letters);
+  console.log(guessedLetters);
+  console.log(wrongLetters);
+
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen startGame={startGame}/>}
-      {gameStage === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameStage === 'game' 
+      && <Game 
+          verifyLetter={verifyLetter}
+          pickedWord={pickedWord}
+          pickedCategory={pickedCategory}
+          letters={letters}
+          guessedLetters={guessedLetters}
+          wrongLetters={wrongLetters}
+          guesses={guesses}
+          score={score}
+        />}
       {gameStage === 'end' && <GameOver retry={retry}/>}
     </div>
   );
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import style from './Game.module.css'
 
 
-const Game = ({verifyLetter}) => {
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score}) => {
+  const [letter, setLetter] = useState('');
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    verifyLetter(letter)
+    
+    setLetter('')
+
+    letterInputRef.current.focus();
+  }
+
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Verificar</button>
+    <div className={style.game}>
+      <p className={style.points}>
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className={style.tip}>
+        Dica sobre a palavra: <p>{pickedCategory}</p>
+      </h3>
+      <p>Você ainda tem {guesses} tentativas</p>
+      <div className={style.wordContainer}>
+        {letters.map((letter, i) =>
+          guessedLetters.includes(letter) ? (
+              <span key={i}className={style.letter}>{letter}</span>
+          ) : (
+              <span key={i} className={style.blankSquare}></span>
+          ))}
+      </div>
+      <div className={style.letterContainer}>
+        <p>Tente adivinhar uma letra da palavra</p>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text' 
+            name='letter' 
+            maxLength='1' 
+            required 
+            onChange={(e) => setLetter(e.target.value)}
+            value={letter}
+            ref={letterInputRef}
+          />
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div>
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) => 
+            <span key={i}>{letter}, </span>
+        )}
+      </div>
     </div>
   )
 }
